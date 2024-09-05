bbbeecf8a0f4762001e32f61dfae1b9e74fe4afd
Bruno <brunoalksouza@gmail.com>
2024-09-05 14:53:42 -0300

    dependencias

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-=======
-import ffmpeg_kit_flutter
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
->>>>>>> 95552045a4f385cb3b01d3608319c1986d7f3339
 }
