e3d8d411b321a5b1eb0c107e945f812a49452119
Bruno <brunoalksouza@gmail.com>
2024-09-07 21:46:35 -0300

    FIX reentruturando arquivo de download para o diretório

--- lib/widgets/image_widget.dart
+++ lib/widgets/image_widget.dart
@@ -1,39 +1,11 @@
 import 'dart:typed_data';
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:mobi_resize_flutter/env.dart';
+import 'package:mobi_resize_flutter/services/save_to_directory.dart';
 
 class ImageWidget extends StatelessWidget {
   final Uint8List? imageBytes;
 
   ImageWidget({this.imageBytes});
-
-  Future<void> _saveImageToFile(
-      BuildContext context, Uint8List imageBytes) async {
-    try {
-      const directoryPath = caminhoPasta;
-      final directoryFile = Directory(directoryPath);
-
-      // Cria o diretório se ele não existir
-      if (!await directoryFile.exists()) {
-        await directoryFile.create(recursive: true);
-      }
-
-      const filePath = '$directoryPath/imagem.png';
-      final file = File(filePath);
-      await file.writeAsBytes(imageBytes);
-
-      // ignore: use_build_context_synchronously
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Imagem salva em $filePath')),
-      );
-    } catch (e) {
-      // ignore: use_build_context_synchronously
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Erro ao salvar a imagem: $e')),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +19,7 @@ class ImageWidget extends StatelessWidget {
             bottom: 10,
             right: 10,
             child: FloatingActionButton(
-              onPressed: () => _saveImageToFile(context, imageBytes!),
+              onPressed: () => SaveToDirectory(context, imageBytes!),
               backgroundColor: Colors.blueGrey,
               tooltip: 'Download',
               child: const Icon(
